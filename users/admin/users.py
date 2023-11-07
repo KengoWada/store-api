0b55fd1c25c47045e227ef4422393107cd906c70
KengoWada <louiskengo3@gmail.com>
2023-11-07 21:43:29 +0300

    Registered user model to admin site.

---
+++ users/admin/users.py
@@ -0,0 +1,74 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import \
+    UserCreationForm as DjangoUserCreationForm
+
+from core.admin import BaseModelAdmin
+from users.models import User
+
+__all__ = ("UserModelAdmin",)
+
+
+class UserCreationForm(DjangoUserCreationForm):
+    class Meta:
+        model = User
+        fields = ("email",)
+
+
+@admin.register(User)
+class UserModelAdmin(UserAdmin, BaseModelAdmin):
+    readonly_fields = ("created_at", "updated_at")
+
+    fieldsets = (
+        (None, {"fields": ("name", "email", "password")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "is_removed",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important Dates", {"fields": ("created_at", "updated_at", "last_login")}),
+    )
+
+    add_form = UserCreationForm
+    add_fieldsets = (
+        (None, {"fields": ("email", "name", "password1", "password2")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "is_removed",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+    )
+
+    list_display = (
+        "email",
+        "name",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+        "is_removed",
+    )
+    list_filter = (
+        "is_staff",
+        "is_superuser",
+        "is_active",
+        "is_removed",
+    )
+
+    search_fields = ("email", "name")
+    ordering = ("email", "name")
