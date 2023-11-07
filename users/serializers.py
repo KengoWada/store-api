e221da3e675af288e9707eb24ca94e8cb4128b88
KengoWada <louiskengo3@gmail.com>
2023-11-07 21:41:07 +0300

    Updated user serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,19 +1,10 @@
 from rest_framework import serializers
 
-from .models import User
+from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = User
-        fields = ('id', 'email', 'first_name', 'last_name', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def create(self, validated_data):
-        email = validated_data.pop('email')
-        password = validated_data.pop('password')
-
-        user = User.objects.create_user(email, password, **validated_data)
-
-        return user
+        fields = ("id", "email", "name", "password")
+        extra_kwargs = {"password": {"write_only": True}}
