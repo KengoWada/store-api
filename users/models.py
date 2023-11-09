06fba3372f6b5f4ef6df009b573c5d945b9b5e90
KengoWada <louiskengo3@gmail.com>
2023-11-09 22:52:45 +0300

    Added user model docstrings

--- users/models.py
+++ users/models.py
@@ -7,6 +7,28 @@ from users.managers import UserManager
 
 
 class User(AbstractUser, BaseModel):
+    """API User model.
+
+    Attributes
+    ----------
+    name : CharField
+        The users name.
+    email : EmailField
+        The users email address.
+    created_at : DateTimeField
+        The timestamp for when the user was created.
+    updated_at : DateTimeField
+        The timestamp for when the user object was last edited.
+    is_removed : BooleanField
+        Indicates that a user object has been soft deleted.
+    is_email_verified : BooleanField
+        Indicates that the users email address has been verified.
+
+    Methods
+    -------
+    get_auth_tokens ()
+        Returns auth tokens for the user.
+    """
     username = None
     date_joined = None
     first_name = None
@@ -28,5 +50,10 @@ class User(AbstractUser, BaseModel):
         return f"{self.email}"
 
     def get_auth_tokens(self):
+        """Returns auth tokens for the user.
+
+        Returns:
+            dict: Returns a dictionary containing access_token and refresh_token.
+        """
         tokens = RefreshToken.for_user(self)
         return {"refresh_token": str(tokens), "access_token": str(tokens.access_token)}
