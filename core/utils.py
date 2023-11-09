b35e7a92bbb9edbd4fa22560c3d0312c5554299b
KengoWada <louiskengo3@gmail.com>
2023-11-09 19:28:10 +0300

    Added core utils for sending email, generating url safe tokens and verifying tokens.

---
+++ core/utils.py
@@ -0,0 +1,52 @@
+from django.conf import settings
+from django.core.mail import EmailMessage
+from itsdangerous import URLSafeTimedSerializer
+from itsdangerous.exc import BadData
+
+FROM_EMAIL = f"Company Name<{settings.EMAIL_HOST_USER}>"
+TOKEN_SERIALIZER = URLSafeTimedSerializer(settings.ITSDANGEROUS_SECRET_KEY)
+
+
+def send_email(subject: str, body: str, to_emails: list[str]):
+    """Send emails from EMAIL_HOST_USER.
+
+    Args:
+        subject (str): The email subject.
+        body (str): The email body.
+        to_emails (list[str]): A list of email addresses to send the email to.
+    """
+    email = EmailMessage(
+        subject=subject, body=body, to=to_emails, from_email=FROM_EMAIL
+    )
+    email.content_subtype = "html"
+    email.send()
+
+
+def generate_token(data: str):
+    """Generate signed token for data provided.
+
+    Args:
+        data (str): Data to be signed.
+
+    Returns:
+        token (str): A signed token for the data provided.
+    """
+    return TOKEN_SERIALIZER.dumps(data, salt=settings.ITSDANGEROUS_SALT_KEY)
+
+
+def verify_token(token: str, expiration=1800):
+    """Verify token validity.
+
+    Args:
+        token (str): Token to be verified.
+        expiration (int, optional): The time in seconds that the token is valid. Defaults to 1800.
+
+    Returns:
+        data (str, None): Data that was signed to the token if valid otherwise returns None.
+    """
+    try:
+        return TOKEN_SERIALIZER.loads(
+            token, salt=settings.ITSDANGEROUS_SALT_KEY, max_age=expiration
+        )
+    except BadData:
+        return None
