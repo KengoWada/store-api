79b772a553377b7b4abaf63be9c0bc9baffa022b
KengoWada <louiskengo3@gmail.com>
2023-11-10 16:40:45 +0300

    Added email verification urls.

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path("register/", views.RegisterUserAPIView.as_view(), name="register-user"),
     path("login/", views.LoginUserAPIView.as_view(), name="login-user"),
     path("token/refresh/", TokenRefreshView.as_view(), name="refresh-token"),
+    path("email/verify/", views.verify_email_address, name="verify-email-address"),
+    path(
+        "email/resend/",
+        views.resend_email_verification_email,
+        name="resend-email-verification-email",
+    ),
 ]
