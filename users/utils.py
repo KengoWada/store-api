c3ef544048300293b11452ee487289647beeb578
KengoWada <louiskengo3@gmail.com>
2023-11-11 16:54:18 +0300

    Updated docstrings

--- users/utils.py
+++ users/utils.py
@@ -3,14 +3,21 @@ from django.template.loader import render_to_string
 from core.utils import generate_token, send_email
 
 
-def send_email_verification_email(name: str, email: str, subject: str, template_name: str):
+def send_email_verification_email(
+    name: str, email: str, subject: str, template_name: str
+):
     """Send email to verify email address provided by the user.
 
-    Args:
-        name (str): Users name.
-        email (str): Users email address.
-        subject (str): Email subject.
-        template_name (str): Email template file name.
+    Parameters
+    ----------
+    name : str
+        The users name.
+    email : str
+        The users email address.
+    subject : str
+        The email subject.
+    template_name : str
+        The email template file name.
     """
     token = generate_token(email)
     context = {"name": name, "verification_link": f"/auth/verify/?token={token}"}
