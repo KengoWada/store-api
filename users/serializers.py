9c1d0720f50d0fda5cfbb04669c9dd465c75ea6c
KengoWada <louiskengo3@gmail.com>
2023-11-11 12:41:57 +0300

    Added password validation.

--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,5 @@
+import re
+
 from rest_framework import serializers
 
 from users.models import User
@@ -8,3 +10,31 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ("id", "email", "name", "password")
         extra_kwargs = {"password": {"write_only": True}}
+
+    def validate_password(self, value):
+        if len(value) < 8:
+            raise serializers.ValidationError(
+                "Password must be at least 8 characters long."
+            )
+
+        if not re.match(r"(?=(?:[^A-Z]*[A-Z]){1})", value):
+            raise serializers.ValidationError(
+                "Password must contain at least 1 upper case character."
+            )
+
+        if not re.match(r"(?=(?:[^a-z]*[a-z]){1})", value):
+            raise serializers.ValidationError(
+                "Password must contain at least 1 lower case character."
+            )
+
+        if not re.match(r"(?=(?:\D*\d){1})", value):
+            raise serializers.ValidationError(
+                "Password must contain at least 1 number."
+            )
+
+        if not re.sub(r"[a-zA-Z0-9]", "", value):
+            raise serializers.ValidationError(
+                "Password must contain at least 1 special character."
+            )
+
+        return value
