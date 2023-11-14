95dad53fe3cfd4f905d91eb9a711eaaba36a0bb5
KengoWada <louiskengo3@gmail.com>
2023-11-14 23:54:22 +0300

    Added tracking user django simple history

--- api/settings/components/base_settings.py
+++ api/settings/components/base_settings.py
@@ -30,6 +30,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "simple_history.middleware.HistoryRequestMiddleware",
 ]
 
 ROOT_URLCONF = "api.urls"
