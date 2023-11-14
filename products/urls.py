2530bf84ffaa19f79bd1129495d63b5510c6e1fb
KengoWada <louiskengo3@gmail.com>
2023-11-14 23:04:16 +0300

    Updated product model views

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,3 @@
+from products.views import urls
 
-from django.urls import path
-
-from .views import ProductCreateList, ProductRetrieveUpdateDelete
-
-urlpatterns = [
-    path('', ProductCreateList.as_view()),
-    path('<int:product_id>/', ProductRetrieveUpdateDelete.as_view()),
-]
+urlpatterns = urls
