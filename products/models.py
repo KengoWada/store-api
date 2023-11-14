dd2abc23ed64754a30ee508ea552ad3c869efcd8
KengoWada <louiskengo3@gmail.com>
2023-11-14 22:58:47 +0300

    Updated product model

--- products/models.py
+++ products/models.py
@@ -1,59 +1,30 @@
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
-STOCK_ACTIONS = [
-    ('create_product', 'Create a new product'),
-    ('update_product_details', 'Update product details'),
-    ('delete_product', 'Delete a product'),
-    ('add_stock', 'Increment the current stock of a product'),
-    ('remove_stock', 'Decrement the current stock of a product'),
-    ('discount_product', 'Add a discount to a product'),
-    ('remove_discount', 'Remove discount from a product'),
-]
+from core.models import BaseModel
 
 
-class Product(models.Model):
+class Product(BaseModel):
+    """Product model."""
 
-    name = models.CharField(max_length=255, null=False)
-    description = models.TextField(null=False)
-    price = models.DecimalField(max_digits=12, decimal_places=2)
-    discount_price = models.DecimalField(
-        max_digits=12, decimal_places=2, default=0.00)
-    quantity = models.IntegerField(default=0)
-    rating = models.IntegerField(default=0)
-    images = ArrayField(models.URLField(), null=False)
-    is_deleted = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    price = models.DecimalField(
+        max_digits=11, decimal_places=2, null=False, blank=False
+    )
+    discount_price = models.DecimalField(max_digits=11, decimal_places=2, default=0.00)
+    quantity = models.IntegerField(null=False, blank=False)
+    images = ArrayField(
+        models.URLField(null=False, blank=False), null=False, blank=False
+    )
 
     class Meta:
-        db_table = 'products'
+        db_table = "products"
+        verbose_name_plural = "Products"
+
+    def __str__(self):
+        return f"{self.pk}: {self.name}"
 
     @property
     def is_discounted(self):
         return self.discount_price > 0
-
-
-class StockRecord(models.Model):
-
-    product = models.ForeignKey('Product', on_delete=models.CASCADE)
-    user = models.ForeignKey('users.User', null=False,
-                             on_delete=models.DO_NOTHING)
-    action = models.CharField(choices=STOCK_ACTIONS,
-                              max_length=255, null=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        db_table = 'stock_records'
-
-
-# class Review(models.Model):
-#     user = models.ForeignKey('users.User', on_delete=models.CASCADE)
-#     product = models.ForeignKey('Products', on_delete=models.CASCADE)
-#     review = models.TextField()
-#     rating = models.DecimalField(max_digits=2, decimal_places=1, default=0.0)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-
-#     class Meta:
-#         db_table = 'product_reviews'
