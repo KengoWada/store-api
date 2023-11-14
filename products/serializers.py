8bec3d07a26ea0a64fc0d65acf705478b1adfcc1
KengoWada <louiskengo3@gmail.com>
2023-11-14 22:57:14 +0300

    Updated product serializer

--- products/serializers.py
+++ products/serializers.py
@@ -1,29 +1,20 @@
 from rest_framework import serializers
 
-from .models import Product, StockRecord
+from products.models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    is_discounted = serializers.ReadOnlyField()
 
     class Meta:
         model = Product
-        fields = ('name', 'description', 'price', 'discount_price',
-                  'quantity', 'images', 'rating', 'is_deleted')
-
-
-class GetProductSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Product
-        fields = ('id', 'name', 'description', 'price', 'discount_price',
-                  'quantity', 'images', 'rating', 'is_discounted')
-
-
-class StockRecordSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = StockRecord
-        fields = ('user', 'product', 'action')
-
-    user = serializers.ReadOnlyField()
-    product = serializers.ReadOnlyField()
+        fields = (
+            "id",
+            "name",
+            "description",
+            "price",
+            "discount_price",
+            "quantity",
+            "images",
+            "is_discounted",
+        )
