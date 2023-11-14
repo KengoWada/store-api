033f21d0eb9ba00bfc80919d723ad549a0490209
KengoWada <louiskengo3@gmail.com>
2023-11-14 22:48:56 +0300

    Added is staff permissions

--- core/permissions.py
+++ core/permissions.py
@@ -1,19 +1,19 @@
-from rest_framework import permissions, status
+from rest_framework import permissions
 
 
 class IsStaffOrReadOnly(permissions.BasePermission):
-    """
-    Custom permission to only allow staff members to certain actions
-    """
+    """Custom permission to only allow staff members to create and update objects."""
 
-    message = {'message': 'Permission denied',
-               'status': status.HTTP_403_FORBIDDEN}
+    message = {"error": "Permission denied"}
 
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
 
         if request.user.is_anonymous:
+            return False
+
+        if request.method == "DELETE":
             return False
 
         return request.user.is_authenticated and request.user.is_staff
@@ -23,6 +23,9 @@ class IsStaffOrReadOnly(permissions.BasePermission):
             return True
 
         if request.user.is_anonymous:
+            return False
+
+        if request.method == "DELETE":
             return False
 
         return request.user.is_authenticated and request.user.is_staff
