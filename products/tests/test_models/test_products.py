29d04aeb5c8c2ec65a82c2818e8a72ece0bc3ee4
KengoWada <louiskengo3@gmail.com>
2023-11-16 17:22:05 +0300

    Added tests for category model

--- products/tests/test_models/test_products.py
+++ products/tests/test_models/test_products.py
@@ -1,6 +1,6 @@
 from rest_framework.test import APITestCase
 
-from products.tests.factories import ProductFactory
+from products.tests.factories import CategoryFactory, ProductFactory
 
 
 class ProductModelTestCase(APITestCase):
@@ -10,12 +10,14 @@ class ProductModelTestCase(APITestCase):
         self.price = 4500.00
         self.quantity = 23
         self.images = ["https://localhost:3000"]
+        self.category = CategoryFactory()
         self.product = ProductFactory(
             name=self.name,
             description=self.description,
             price=self.price,
             quantity=self.quantity,
             images=self.images,
+            category=self.category,
         )
 
     def test_fields(self):
@@ -25,7 +27,25 @@ class ProductModelTestCase(APITestCase):
         self.assertEqual(self.product.quantity, self.quantity)
         self.assertFalse(self.product.is_discounted)
         self.assertCountEqual(self.product.images, self.images)
+        self.assertEqual(self.product.category.pk, self.category.pk)
 
     def test_string_representation(self):
         string_representation = f"{self.product.pk}: {self.product.name}"
         self.assertEqual(str(self.product), string_representation)
+
+
+class CategoryModelTestCase(APITestCase):
+    def setUp(self):
+        self.name = "Category Name"
+        self.description = "Category Description"
+        self.category = CategoryFactory(
+            name=self.name, description=self.description, is_removed=False
+        )
+
+    def test_fields(self):
+        self.assertEqual(self.category.name, self.name)
+        self.assertEqual(self.category.description, self.description)
+        self.assertFalse(self.category.is_removed)
+
+    def test_string_representation(self):
+        self.assertEqual(str(self.category), self.name)
