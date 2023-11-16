cfc33edd039be3c5fa4a7e46b4cbbe7839c5c586
KengoWada <louiskengo3@gmail.com>
2023-11-16 17:22:42 +0300

    Added category model factory

--- products/tests/factories/products.py
+++ products/tests/factories/products.py
@@ -1,8 +1,8 @@
 import factory
 
-from products.models import Product
+from products.models import Category, Product
 
-__all__ = ("ProductFactory",)
+__all__ = ("ProductFactory", "CategoryFactory")
 
 
 class ProductFactory(factory.django.DjangoModelFactory):
@@ -12,6 +12,16 @@ class ProductFactory(factory.django.DjangoModelFactory):
     quantity = 15
     images = ["https://image.com"]
     is_removed = factory.Faker("boolean", chance_of_getting_true=25)
+    category = factory.SubFactory("products.tests.factories.products.CategoryFactory")
 
     class Meta:
         model = Product
+
+
+class CategoryFactory(factory.django.DjangoModelFactory):
+    name = factory.Sequence(lambda n: f"category name {n}")
+    description = factory.Sequence(lambda n: f"category description {n}")
+    is_removed = factory.Faker("boolean", chance_of_getting_true=25)
+
+    class Meta:
+        model = Category
