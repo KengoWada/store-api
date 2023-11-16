aa7cee606de11008bbbcb4d4d44ee877b41c087d
KengoWada <louiskengo3@gmail.com>
2023-11-16 17:23:17 +0300

    Added tests for category model serializers

--- products/tests/test_serializers.py
+++ products/tests/test_serializers.py
@@ -1,12 +1,14 @@
 from rest_framework.test import APITestCase
 
+from products.models import Product
 from products.serializers import ProductSerializer
-from products.tests.factories import ProductFactory
+from products.tests.factories import CategoryFactory, ProductFactory
 
 
 class ProductSerializerTestCase(APITestCase):
     def setUp(self):
-        self.product = ProductFactory()
+        self.category = CategoryFactory(is_removed=False)
+        self.product = ProductFactory(category=self.category)
 
     def test_fields(self):
         serializer = ProductSerializer(self.product)
@@ -22,3 +24,20 @@ class ProductSerializerTestCase(APITestCase):
             serializer.data["discount_price"], f"{self.product.discount_price:.2f}"
         )
         self.assertFalse(serializer.data["is_discounted"])
+        self.assertEqual(serializer.data["category"], self.category.name)
+
+    def test_create_product(self):
+        data = {
+            "name": "Product Name",
+            "description": "Product description",
+            "price": "1200.00",
+            "quantity": 23,
+            "images": ["https://localhost:3000/image.webp"],
+            "category": self.category.pk,
+        }
+        serializer = ProductSerializer(data=data)
+        serializer.is_valid()
+        self.assertFalse(serializer.errors)
+        serializer.save()
+
+        self.assertTrue(Product.objects.filter(pk=serializer.data["id"]).exists())
